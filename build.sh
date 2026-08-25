16a14f8011393aa0ffd51c50ea93b28c28a39f7c
jd070908 <josedavid070908@gmail.com>
2026-08-24 21:44:52 -0400

    Agregar creación automática de superusuario en build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
+# Instalar dependencias
 pip install -r requirements.txt
 
+# Recoger archivos estáticos
 python manage.py collectstatic --no-input
+
+# Aplicar migraciones a PostgreSQL
 python manage.py migrate
+
+# Crear superusuario automático (si no existe)
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='josed').exists():
+    User.objects.create_superuser('josed', 'jose070908@gmail.com', 'jose070908')
+    print('Superusuario creado exitosamente.')
+else:
+    print('El superusuario ya existe.')
+"
